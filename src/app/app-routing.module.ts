16044cf0c0ea1e01d8dea24a018cd6da72dfe451
Idriisss <idrissbhh@gmail.com>
2025-05-08 21:27:32 +0100

    EvaluationModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { AppLayoutComponent } from './layout/app.layout.component';
-
+import { EvaluationComponent } from './demo/components/evaluation/evaluation.component';
 const routerOptions: ExtraOptions = {
     anchorScrolling: 'enabled'
 };
@@ -11,6 +11,7 @@ const routes: Routes = [
     {
         path: '', component: AppLayoutComponent,
         children: [
+            { path: 'evaluation', component: EvaluationComponent },
             { path: '', loadChildren: () => import('./demo/components/dashboards/dashboards.module').then(m => m.DashboardsModule) },
             { path: 'uikit', data: { breadcrumb: 'UI Kit' }, loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
             { path: 'utilities', data: { breadcrumb: 'Utilities' }, loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
